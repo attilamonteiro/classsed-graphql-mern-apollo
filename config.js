a98098e9e38bf6ddb2e5649996b370b516ec57bb
attilamonteiro <atmaload@gmail.com>
2023-04-10 17:17:10 -0300

    att

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
-  SECRET_KEY: 'some very secret key'
+    'mongodb+srv://atmaload:Wry1p1QqVEJLAoaE@cluster0.ltjehoa.mongodb.net/?retryWrites=true&w=majority',
+  SECRET_KEY: 'Wry1p1QqVEJLAoaE'
 };
